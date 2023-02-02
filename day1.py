caf7f7815e599bd2af08a184cddc0b830997b609
SundayEfeTerry <info.techbro.set@gmail.com>
2023-02-02 11:39:58 +0100

    day_01
    This is day 01

---
+++ day1.py
@@ -0,0 +1,14 @@
+#Welcome to day 1 of this challange
+#This is how you write a comment
+"""This is also multi line comment"""
+
+print("Hello.. this is a simple print out!")
+
+"""Multi line format print out"""
+
+print("""
+    Hello
+        From
+            Multi
+                Line
+""")
